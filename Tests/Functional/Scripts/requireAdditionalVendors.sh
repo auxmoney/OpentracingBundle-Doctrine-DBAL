c7e8d27d94887b049b7a69de6ac3da03eccf3ef5
Christian Alexander Wolf <mail@cawolf.de>
2020-06-17 07:08:14 +0200

    fix: do not use github api in travis

--- Tests/Functional/Scripts/requireAdditionalVendors.sh
+++ Tests/Functional/Scripts/requireAdditionalVendors.sh
@@ -2,6 +2,7 @@
 
 cd build/testproject/
 composer config repositories.origin vcs https://github.com/${PR_ORIGIN}
+composer config use-github-api false
 composer require auxmoney/opentracing-bundle-doctrine-dbal:dev-${BRANCH} webmozart/assert --no-scripts
 composer fix-recipes
 cd ../../
