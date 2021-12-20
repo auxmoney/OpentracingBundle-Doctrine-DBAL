93f8c7d7bd7f5314a924d67c44208efe7c4a878b
Christian A. Wolf <mail@cawolf.de>
2021-12-20 18:12:12 +0100

    fix: disable SF 3.4 for testing

--- Tests/Functional/Scripts/requireORMAndCreateResetPoint.sh
+++ Tests/Functional/Scripts/requireORMAndCreateResetPoint.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 cd build/testproject/
-if [ "$SYMFONY_VERSION" == "3.4.*" ];
-then
-    composer require doctrine/orm:2.7.5 --no-scripts
-else
-    composer require doctrine/orm --no-scripts
+VENDOR_VERSION=""
+CURRENT_REF=${GITHUB_HEAD_REF:-$GITHUB_REF}
+CURRENT_BRANCH=${CURRENT_REF#refs/heads/}
+if [[ $CURRENT_BRANCH -ne "master" ]]; then
+    composer config minimum-stability dev
+    VENDOR_VERSION=":dev-${CURRENT_BRANCH}"
 fi
+composer require doctrine/orm${VENDOR_VERSION} --with-all-dependencies --no-scripts
 git add .
 git commit -m"add orm"
 git tag reset.orm
