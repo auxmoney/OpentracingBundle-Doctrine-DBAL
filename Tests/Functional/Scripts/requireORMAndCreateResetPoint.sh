da235e692ea5454fe43d46d39cd3a5fc90e21e55
Christian A. Wolf <mail@cawolf.de>
2021-12-20 18:16:18 +0100

    fix: do not use repository branch for orm

--- Tests/Functional/Scripts/requireORMAndCreateResetPoint.sh
+++ Tests/Functional/Scripts/requireORMAndCreateResetPoint.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
 
 cd build/testproject/
-VENDOR_VERSION=""
-CURRENT_REF=${GITHUB_HEAD_REF:-$GITHUB_REF}
-CURRENT_BRANCH=${CURRENT_REF#refs/heads/}
-if [[ $CURRENT_BRANCH -ne "master" ]]; then
-    composer config minimum-stability dev
-    VENDOR_VERSION=":dev-${CURRENT_BRANCH}"
-fi
-composer require doctrine/orm${VENDOR_VERSION} --with-all-dependencies --no-scripts
+composer require doctrine/orm --with-all-dependencies --no-scripts
 git add .
 git commit -m"add orm"
 git tag reset.orm
